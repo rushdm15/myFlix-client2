e9e4311a8ca18d208c5311e883d7081d5cfbf14c
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-10-09 18:55:54 +0200

    edited main-view

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            movies: [
+                { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
+                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
+                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}    
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="main-view">
+                <div>Inception</div>
+                <div>The Shawshank Redemption</div>
+                <div>Gladiator</div>
+            </div>
+        );
+    }
+}
+
+// export default MainView;
