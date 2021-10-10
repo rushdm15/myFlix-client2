4e1a88352d85278409287bf616ca18547b0bb47c
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-10-10 16:11:15 +0200

    edit

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { MovieCard } from '../movie-card/movie-card';
 
 export class MovieCard extends React.Component {
     render() {
         const { movie, onMovieClick } = this.props;
-        return <div className="movie-card" onClick={() => {onMovieClick(movie); }}>{movie.Title}</div>;
+        return <div className="movie-card" onClick={() => 
+            {onMovieClick(movie); }}>{movie.Title}</div>;
     }
 }
