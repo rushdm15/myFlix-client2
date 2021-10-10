342453c6a1c0b5324a92cc21c4590c6b50d8c3f8
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-10-10 13:19:48 +0200

    create movie-card component

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+
+export class MovieCard extends React.Component {
+    render() {
+        return <div className="movie-card">some title</div>;
+    }
+}
