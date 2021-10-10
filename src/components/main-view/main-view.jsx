a4f1d6d451005395a8cfe3caaa7b8b3047940f73
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-10-10 13:26:39 +0200

    key,  movie

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -21,7 +21,7 @@ export class MainView extends React.Component {
         
             return (
             <div className="main-view">
-                {movies.map(movie => <MovieCard />)}
+                {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
             </div>
         );
     }
